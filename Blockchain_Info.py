9efa6de49c279652275c75f64e576c155e5592ef
Bnbaotam <45367121+Bnbaotam@users.noreply.github.com>
2021-12-14 13:55:54 +0700

    Update Blockchain_Info.py
    Thêm ví dụ về  transaction chuyển tiền và kiểm tra số dư

--- Blockchain_Info.py
+++ Blockchain_Info.py
@@ -2,3 +2,5 @@ ADAFRUIT_IO_KEY = 'aio_wQbm35FrNhPIEYNXpF8LthdYS5d2'
 ADAFRUIT_IO_USERNAME = 'bnbaotam'
 Main_net = 'main-net'
 Transaction_net = 'transactions-net'
+#Ví dụ 1 transaction chuyển tiền: {"lenh": "chuyen_tien", "nguoi_gui": "Tam", "nguoi_nhan": "Mike", "so_tien": "10"}
+#Ví dụ 1 transaction kiểm tra số dư: {"lenh": "so_du", "nguoi_gui": "Mike"}
